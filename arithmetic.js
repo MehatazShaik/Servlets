53bcbd93002e369ed2b9fe74bf5ba812a684bb18
MehatazShaik <76908411+MehatazShaik@users.noreply.github.com>
2021-01-05 11:08:15 +0530

    Add files via upload

---
+++ arithmetic.js
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+function add(){
+	var num1,num2,sum;
+	num1=parseInt(prompt("Enter 1st no : ",""));
+	num2=parseInt(prompt("Enter 2nd no : ",""));
+	
+	sum=num1+num2;
+	
+	document.write("sum = "+sum);
+
+}
+function multiply(){
+	var num1,num2,product;
+	num1=parseInt(prompt("Enter 1st no : ",""));
+	num2=parseInt(prompt("Enter 2nd no : ",""));
+	
+	product=num1*num2;
+	
+	document.write("product = "+product);
+
+}
